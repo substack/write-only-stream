3c000ad31b92fcde93b9f543d1205d110bd7dc8c
James Halliday <mail@substack.net>
2014-09-14 22:47:53 +0300

    passing test

---
+++ index.js
@@ -0,0 +1,20 @@
+var Writable = require('readable-stream/writable');
+
+module.exports = function (stream) {
+    var opts = stream._writableState;
+    var compat = typeof stream._write !== 'function';
+    var wo = new Writable({ objectMode: opts && opts.objectMode });
+    
+    wo._write = function (buf, enc, next) {
+        if (compat) {
+            stream.write(buf);
+            next();
+        }
+        else stream._write(buf, enc, next);
+    };
+    
+    wo.once('finish', function () {
+        stream.end();
+    });
+    return wo;
+};
